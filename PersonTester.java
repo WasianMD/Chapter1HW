cdf7a6d4551cb34aec46ac84e42853cf1340d08f
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-09-13 20:22:39 -0400

    Create PersonTester.java

---
+++ PersonTester.java
@@ -0,0 +1,11 @@
+public class PersonTester {
+    public static void main(String[] args){
+        Person person1 = new Person();
+        person1.setName = "Howard Roark";
+        person1.setAge = 24;
+        person1.setEyeColor = "Green"
+
+
+        person1.welcomeMessage();
+    }
+}

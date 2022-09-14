e4dfbce5ad01427e5e36b1f79fd2b50aed33ec1b
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-09-13 20:21:17 -0400

    Create Bike.java

---
+++ Bike.java
@@ -0,0 +1,26 @@
+public class Bike {
+    
+    private int wheels;
+    private String manufacturer;
+    private int year;
+
+    public int getWheels(){
+        return wheels;
+    }
+    public void setWheels(int)
+    public String getManufacturer(){
+        return manufacturer;
+    }
+    public void setManufacturer(String)
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int)
+        
+
+    public String toString(){
+        return "Manufacturer: " + manufacturer + "\n" + "Wheels: " + wheels + "\n" + "Year: " + year + "\n"
+    }
+
+}
+}

82a27ada45b3b06ac8269b298103902e73f5a7f1
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-09-13 20:21:42 -0400

    Create BikeTester.java

---
+++ BikeTester.java
@@ -0,0 +1,11 @@
+public class BikeTester {
+    public static void main(String[] args){
+        Bike bike1 = new Bike();
+        bike1.setWheels = 2;
+        bike1.setManufacturer = "Schwinn";
+        bike1.setYear = 2014;
+
+
+        return toString();
+    }
+}
